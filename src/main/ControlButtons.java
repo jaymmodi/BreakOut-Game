4c4653d2d125f83624f8f209effcec01a412f398
JAY <jmmodi@indiana.edu>
2015-02-05 19:41:34 -0500

    JIRA: http://tintin.cs.indiana.edu:8091/browse/TEAMFOUR-9 Save Button Added.

--- src/main/ControlButtons.java
+++ src/main/ControlButtons.java
@@ -17,14 +17,13 @@ import javax.swing.JPanel;
 public class ControlButtons extends JPanel {
 
 	private Command theCommand;
-	private int gameState; 
+	private int gameState;
 	private GameDriver gameDriver;
 	private GameBoard game;
 	private DisplayClock clock;
 	private TimerObservable timerObs;
 	private boolean isPaused;
 	private boolean isStart;
-
 
 	public boolean isPaused() {
 		return isPaused;
@@ -66,30 +65,31 @@ public class ControlButtons extends JPanel {
 		this.clock = clock;
 	}
 
-	public void press(){
+	public void press() {
 		theCommand.execute();
 	}
 
-	public void setGameDriver(GameDriver gameDriver){
+	public void setGameDriver(GameDriver gameDriver) {
 		this.gameDriver = gameDriver;
 	}
-	public GameDriver getGameDriver(){
+
+	public GameDriver getGameDriver() {
 		return gameDriver;
 	}
 
-	public int getGameState(){
+	public int getGameState() {
 		return gameState;
 	}
 
-	public void setGameState(int gameState){
+	public void setGameState(int gameState) {
 		this.gameState = gameState;
 	}
 
-	public Command getTheCommand(){
+	public Command getTheCommand() {
 		return theCommand;
 	}
 
-	public void setTheCommand(Command theCommand){
+	public void setTheCommand(Command theCommand) {
 		this.theCommand = theCommand;
 	}
 
@@ -97,7 +97,8 @@ public class ControlButtons extends JPanel {
 	JButton st_pse = new JButton("Pause");
 	JButton st_undo = new JButton("Undo");
 	JButton st_replay = new JButton("Replay");
-	
+	JButton st_save = new JButton("Save");
+	JButton st_load = new JButton("Load");
 
 	public ControlButtons(final GameBoard game) {
 		setStart(false);
@@ -107,54 +108,54 @@ public class ControlButtons extends JPanel {
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
 		layout.setHorizontalGroup(layout.createSequentialGroup()
-				.addComponent(st_but)
-				.addComponent(st_pse)
-				.addComponent(st_undo)
-				.addComponent(st_replay));
+				.addComponent(st_but).addComponent(st_pse)
+				.addComponent(st_undo).addComponent(st_replay)
+				.addComponent(st_save).addComponent(st_load));
 
-		layout.setVerticalGroup(
-				layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-				.addComponent(st_but))
-				.addComponent(st_pse)
-				.addComponent(st_undo)
-				.addComponent(st_replay));
-		
+		layout.setVerticalGroup(layout
+				.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addGroup(layout.createSequentialGroup().addComponent(st_but))
+				.addComponent(st_pse).addComponent(st_undo)
+				.addComponent(st_replay).addComponent(st_save)
+				.addComponent(st_load));
+
 		st_pse.setEnabled(false);
 		st_undo.setEnabled(false);
 		st_replay.setEnabled(false);
-		
+		st_save.setEnabled(false);
+
 		st_but.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
 				st_pse.setEnabled(true);
 				st_undo.setEnabled(true);
-				st_replay.setEnabled(true);              
+				st_replay.setEnabled(true);
+				st_save.setEnabled(true);
 				game.requestFocusInWindow();
 
-				if(st_but.getText().equals("Restart"))
-				{
+				if (st_but.getText().equals("Restart")) {
 					st_but.setText("Start");
 					st_pse.setText("Pause");
 					timerObs.getTimer().stop();
 					timerObs = new TimerObservable();
 					setTimerObs(timerObs);
 					timerObs.addObserver((Observer) gameDriver.getGameBoard());
-					timerObs.addObserver((Observer) gameDriver.getDisplayClock());
+					timerObs.addObserver((Observer) gameDriver
+							.getDisplayClock());
 				}
 
-				else if (st_but.getText().equals("Start"))
-				{   
-					StartCommand startCmd;   
+				else if (st_but.getText().equals("Start")) {
+					StartCommand startCmd;
 					startCmd = new StartCommand(timerObs);
 					timerObs.addObserver((Observer) gameDriver.getGameBoard());
-					timerObs.addObserver((Observer) gameDriver.getDisplayClock());
+					timerObs.addObserver((Observer) gameDriver
+							.getDisplayClock());
 					gameDriver.getControlButtons().setTheCommand(startCmd);
 					gameDriver.getControlButtons().press();
 					st_but.setText("Restart");
 				}
 			}
-			
+
 		});
 
 		st_pse.addActionListener(new ActionListener() {
@@ -162,31 +163,33 @@ public class ControlButtons extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
 				st_but.setEnabled(true);
-				if (timerObs.getComputeCoordinatesObj().getGameFlag()==2){
+				//st_save.setEnabled(true);
+				// st_load.setEnabled(false);
+				if (timerObs.getComputeCoordinatesObj().getGameFlag() == 2) {
 					st_but.setEnabled(true);
-					st_pse.setEnabled(false); 
+					st_pse.setEnabled(false);
 					st_undo.setEnabled(false);
 					st_replay.setEnabled(false);
 					timerObs.getTimer().stop();
-                	timerObs.deleteObservers();
+					timerObs.deleteObservers();
 				}
-                	
-				if (st_pse.getText().equals("Pause"))
-				{                	
-					PauseCommand pauseCmd;   
+
+				if (st_pse.getText().equals("Pause")) {
+					PauseCommand pauseCmd;
 					pauseCmd = new PauseCommand(timerObs);
-					timerObs.deleteObserver((Observer) gameDriver.getGameBoard());
-					timerObs.deleteObserver((Observer) gameDriver.getDisplayClock());
+					timerObs.deleteObserver((Observer) gameDriver
+							.getGameBoard());
+					timerObs.deleteObserver((Observer) gameDriver
+							.getDisplayClock());
 					gameDriver.getControlButtons().setTheCommand(pauseCmd);
 					gameDriver.getControlButtons().press();
 					st_pse.setText("Resume");
-				}
-				else
-				{
+				} else {
 					ResumeCommand resumeCmd;
 					resumeCmd = new ResumeCommand(timerObs);
 					timerObs.addObserver((Observer) gameDriver.getGameBoard());
-					timerObs.addObserver((Observer) gameDriver.getDisplayClock());
+					timerObs.addObserver((Observer) gameDriver
+							.getDisplayClock());
 					gameDriver.getControlButtons().setTheCommand(resumeCmd);
 					gameDriver.getControlButtons().press();
 					game.requestFocusInWindow();
@@ -195,23 +198,21 @@ public class ControlButtons extends JPanel {
 			}
 		});
 
-
 		st_undo.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
 				st_but.setEnabled(true);
-				st_pse.setEnabled(true); 
+				st_pse.setEnabled(true);
 				st_undo.setEnabled(true);
 				st_replay.setEnabled(true);
 				st_pse.setText("Resume");
 				isStart = false;
-				UndoCommand undoCmd;   
+				UndoCommand undoCmd;
 				undoCmd = new UndoCommand(timerObs);
 				gameDriver.getControlButtons().setTheCommand(undoCmd);
 				gameDriver.getControlButtons().press();
 			}
 		});
-
 
 		st_replay.addActionListener(new ActionListener() {
 			@Override
@@ -222,13 +223,23 @@ public class ControlButtons extends JPanel {
 				st_undo.setEnabled(false);
 				st_replay.setEnabled(false);
 				isStart = false;
-				ReplayCommand replyCmd;   
+				ReplayCommand replyCmd;
 				replyCmd = new ReplayCommand(timerObs);
 				gameDriver.getControlButtons().setTheCommand(replyCmd);
 				gameDriver.getControlButtons().press();
 				st_undo.setEnabled(true);
 				st_replay.setEnabled(true);
+
+			}
+		});
+		st_save.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
 				
+			
+			
 			}
 		});
 		setBackground(Color.black);
