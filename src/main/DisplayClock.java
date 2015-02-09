f5535a89bb1ecd052d89ee2113b819f8fe3852a5
Amritanshu Joshi <amrijosh@indiana.edu>
2015-02-08 22:49:36 -0500

    Refactoring code according to style guide.
    JIRA: http://tintin.cs.indiana.edu:8091/browse/TEAMFOUR-11

--- src/main/DisplayClock.java
+++ src/main/DisplayClock.java
@@ -15,7 +15,8 @@ import javax.swing.JPanel;
  * Class DisplayClock - this acts as the Observer and updates itself after every
  * 1 second .
  * 
- *
+ * update() - updates DisplayClock observer according to notification from
+ * observable
  */
 
 public class DisplayClock implements Observer {
@@ -35,9 +36,9 @@ public class DisplayClock implements Observer {
 	 * Method - depending on the object received it updates itself.
 	 */
 	@Override
-	public void update(Observable o, Object objList) {
-		if (!(objList instanceof LinkedList<?>)) {
-			ArrayList<Object> obj = (ArrayList<Object>) objList;
+	public void update(Observable oservable, Object objectList) {
+		if (!(objectList instanceof LinkedList<?>)) {
+			ArrayList<Object> obj = (ArrayList<Object>) objectList;
 			if (obj.get(0) instanceof Number
 					&& ((Integer) obj.get(0)).intValue() == 2)
 				time.setText("00:00");
