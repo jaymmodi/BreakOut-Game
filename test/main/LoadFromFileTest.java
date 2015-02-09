1bae09f2fd40e3f90ffd533297afd27dc97ea7bf
Amritanshu Joshi <amrijosh@indiana.edu>
2015-02-08 22:58:20 -0500

    Fixing bamboo buid issue. Refactoring code according to style guide. JIRA: http://tintin.cs.indiana.edu:8091/browse/TEAMFOUR-11

--- test/main/LoadFromFileTest.java
+++ test/main/LoadFromFileTest.java
@@ -11,8 +11,6 @@ import java.util.LinkedList;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
 public class LoadFromFileTest {
 
 	LoadFromFile loadFromFile;
